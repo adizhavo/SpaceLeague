5ccb001740cfe3e2f8487cf3b0b184dd071fe982
Adi Zhavo <adi@gram.gs>
2016-12-11 15:17:08 +0200

    Add camera shakes

---
+++ Assets/Scripts/Ship/Camera/Shake/CameraShakeTransform.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace SpaceLeague.CameraShake
+{
+    public class CameraShakeTransform : MonoBehaviour
+    {    
+        public float LerpSpeed;
+
+        private void Update () 
+        {
+            CameraShakeProvider.Instance.FrameFeed();
+            Vector2 shakePoint = CameraShakeProvider.Instance.GetShakePoint();
+            Vector3 localShakePoint = new Vector3(shakePoint.x, shakePoint.y, transform.localPosition.z);
+            transform.localPosition = Vector3.Lerp(transform.localPosition, localShakePoint, LerpSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Ship/Camera/Shake/ShakeType.cs
@@ -0,0 +1,10 @@
+﻿namespace SpaceLeague.CameraShake
+{
+    public enum ShakeType
+    {
+        Fire = 1,
+        Hit = 2,
+        CloseExplosion = 3,
+        DistantExplosion = 4
+    }
+}
