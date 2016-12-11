876179e87ef4d6e1aec6063c60f918d5c6c4c43b
Adi Zhavo <adi@gram.gs>
2016-12-11 15:15:56 +0200

    Add ship main cannon

---
+++ Assets/Scripts/Ship/Weapon/PlayerShipMainCannon.cs
@@ -0,0 +1,13 @@
+﻿using SpaceLeague.CameraShake;
+
+namespace SpaceLeague.Ship.Weapon
+{
+    public class PlayerShipMainCannon : ShipMainCannon
+    {
+        protected override void Fire()
+        {
+            base.Fire();
+            CameraShakeProvider.Instance.StartShake(ShakeType.Fire);
+        }
+    }
+}
---
+++ Assets/Scripts/Ship/Weapon/ShipMainCannon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using SpaceLeague.Ship;
+using SpaceLeague.Pooling;
+
+namespace SpaceLeague.Ship.Weapon
+{
+    public class ShipMainCannon : MonoBehaviour
+    {
+        [SerializeField] protected Ship ship;
+        [SerializeField] protected float fireRate;
+        [SerializeField] [Range(0f, 10f)] protected float accuracy;
+        [SerializeField] protected Transform weaponPivot;
+
+        protected float timeCounter;
+
+        private void Start()
+        {
+            timeCounter = fireRate;
+        }
+
+        public void OpenFire()
+        {
+            timeCounter += Time.deltaTime;
+            if (timeCounter > fireRate) Fire();
+        }
+
+        protected virtual void Fire()
+        {
+            Transform bullet = PoolProvider.Instance.RequestGameObject(PooledObject.Bullet).transform;
+            bullet.position = weaponPivot.position;
+            Vector3 offset = Random.onUnitSphere * (10f - accuracy);
+            bullet.LookAt(ship.GlobalDirection + offset, weaponPivot.up);
+            bullet.GetComponent<Bullet>().fireSource = ship.transform;
+            timeCounter = 0f;
+        }
+    }
+}
