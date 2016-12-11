1544274e1861d66693f42f2d5549dd1521aa85b0
Adi Zhavo <adi@gram.gs>
2016-12-11 15:16:08 +0200

    Add ship bullet

---
+++ Assets/Scripts/Ship/Weapon/Bullet.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+namespace SpaceLeague.Ship.Weapon
+{
+    public class Bullet : MonoBehaviour 
+    {
+        [SerializeField] private float damage;
+        [SerializeField] private float movementSpeed;
+        [SerializeField] private float lifeTime;
+
+        private float timeCounter = 0f;
+
+        private Vector3? previousPos;
+        private RaycastHit hitObject;
+
+        [HideInInspector] public Transform fireSource;
+
+    	private void Update() 
+        {
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime, Space.Self);
+
+            timeCounter += Time.deltaTime;
+            if (timeCounter > lifeTime)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
+
+            CheckCollisions();
+    	}
+
+        private void OnDisable()
+        {
+            timeCounter = 0f;
+            previousPos = null;
+        }
+
+        private void CheckCollisions()
+        {
+            if (!previousPos.HasValue)
+            {
+                previousPos = transform.position;
+                return;
+            }
+
+            if (Physics.Linecast(previousPos.Value, transform.position, out hitObject))
+            {
+                HitTarget(hitObject.transform.root);
+            }
+
+            previousPos = transform.position;
+        }
+
+        private void HitTarget(Transform tr)
+        {
+            if (fireSource != null && tr.Equals(fireSource)) return;
+
+            Ship s = tr.GetComponent<Ship>();
+            if (s != null) s.Damaged(fireSource, damage);
+
+            gameObject.SetActive(false);
+        }
+    }
+}
