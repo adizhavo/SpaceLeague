ffe2daa246a3bed5be401e5cae94c235bbfc7f99
Adi Zhavo <adi@gram.gs>
2016-12-11 15:14:53 +0200

    Add player ship

---
+++ Assets/Scripts/Ship/Player/PlayerShip.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using SpaceLeague.Ship;
+using SpaceLeague.CameraShake;
+
+namespace SpaceLeague.Ship.Player
+{
+    public class PlayerShip : Ship 
+    {    
+        [SerializeField] private RectTransform AimUI;
+        [HideInInspector] public float AimSensibility = 1f;
+
+        public Transform targetShip;
+
+        private void Start()
+        {
+            #if !UNITY_EDITOR
+            AimSensibility /= 80f;
+            #endif
+
+            Init(movementSpeed, 0.62f, shipCamera);
+        }
+
+        public void Init(float aimSensibility, float movementSpeed, float rotationAngleStepPercentage, Transform shipCamera = null)
+        {
+            this.AimSensibility = aimSensibility > 0f ? aimSensibility : 0f;
+            base.Init(movementSpeed, rotationAngleStepPercentage, shipCamera);
+        }
+
+        private void Update()
+        {
+            PositionAimUI();
+
+            #if PLAYER_SHIP_LOG
+            DisplayLogs();
+            #endif
+        }
+
+        private void FixedUpdate () 
+        {
+            UpdateShipDirection();
+            MoveShipForward();
+            UpdateCameraPosition();
+        }
+
+        private void PositionAimUI()
+        {
+            Vector3 screenPos = Camera.main.WorldToScreenPoint(GlobalDirection);
+            AimUI.position = screenPos;
+        }
+
+        public override void Damaged(Transform attackingShip, float damage)
+        {
+            #if UNITY_EDITOR
+            Debug.Log("Player damaged by " + attackingShip.name);
+            #endif
+            CameraShakeProvider.Instance.StartShake(ShakeType.Hit);
+        }
+    }
+}
